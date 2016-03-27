7ddb9e1d0863cd8c715e5215b90d04bc7f7db6e6
Dax Sorbito <daxsorbito@gmail.com>
2016-03-27 08:24:51 +0800

    implemented indexRoute

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
 
-const Home = () => <div><h1>Home</h1><Links /></div>;
-const About = () => <div><h1>About</h1><Links /></div>;
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
+const Main = (props) => <div><h1>Our Site</h1><Links />{props.children}</div>;
+const About = () => <div><h1>About</h1></div>;
+const Contact = () => <div><h1>Contact</h1></div>;
 
 const Links = () =>
   <nav>
@@ -16,9 +16,10 @@ class App extends React.Component {
   render() {
     return (
       <Router history={ hashHistory }>
-        <Route path="/" component={Home}></Route>
-        <Route path="/about" component={About}></Route>
-        <Route path="/contact" component={Contact}></Route>
+        <Route path="/" component={Main}>
+          <IndexRoute component={About}></IndexRoute>
+          <Route path="contact" component={Contact}></Route>
+        </Route>
       </Router>
     );
   }
